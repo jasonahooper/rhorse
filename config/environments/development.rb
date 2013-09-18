b8d7952d736cd1e703fc5cac774bb6007b22c848
Jason Hooper <jason.hooper@talk21.com>
2013-09-18 15:04:36 +0100

    simple auth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,9 @@ Rhorse::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.middleware.use Rack::Auth::Basic, "Beta Access" do |username, password|
+    'rodney' == username
+    'horsemanager' == password
+  end
 end
